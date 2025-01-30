b07cb8b4426ee94e692a2f7b540c821ddd4f210e
fretzestavillo <estavillofretz@gmail.com>
2025-01-30 10:33:58 +0800

    test deploy 1

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`

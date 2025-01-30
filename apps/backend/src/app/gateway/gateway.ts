e1080e040288ee115d3b42c7e708dcec7b1ec24a
fretzestavillo <estavillofretz@gmail.com>
2025-01-30 18:30:23 +0800

    test

--- apps/backend/src/app/gateway/gateway.ts
+++ apps/backend/src/app/gateway/gateway.ts
@@ -16,7 +16,7 @@ import {
 import { Inject, Logger } from '@nestjs/common';
 import { ChatService } from '../chat.service';
 
-@WebSocketGateway(3002, { cors: { origin: '*' } })
+@WebSocketGateway({ cors: { origin: '*' } })
 export class MyGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
--- apps/web/src/app/Chat/socket.tsx
+++ apps/web/src/app/Chat/socket.tsx
@@ -1,11 +1,8 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socketUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://chat-call-app-backend-service-422041495987.asia-southeast1.run.app'
-    : 'http://localhost:3002';
-
-export const socket = io(socketUrl);
+export const socket = io(
+  'https://chat-call-app-backend-service-422041495987.asia-southeast1.run.app'
+);
 export const WebsocketContext = createContext<Socket>(socket);
 export const WebsocketProvider = WebsocketContext.Provider;

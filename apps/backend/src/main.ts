fa273028a4c928dc00fb084512633d04d8adef5b
fretzestavillo <estavillofretz@gmail.com>
2025-01-30 18:12:17 +0800

    test

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,15 @@ const HOST = '0.0.0.0';
 const PORT = Number(process.env.PORT) || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
-  app.enableCors();
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: 'https://web-service-422041495987.asia-southeast1.run.app', // Replace with the frontend's URL in production
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    });
+  } else {
+    app.enableCors();
+  }
   await app.listen(PORT, HOST).then(() => {
     Logger.log(
       `** Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/ **`
--- apps/web/src/app/Chat/socket.tsx
+++ apps/web/src/app/Chat/socket.tsx
@@ -1,8 +1,11 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-export const socket = io(
-  'https://chat-call-app-backend-service-422041495987.asia-southeast1.run.app'
-);
+const socketUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://chat-call-app-backend-service-422041495987.asia-southeast1.run.app'
+    : 'http://localhost:3002';
+
+export const socket = io(socketUrl);
 export const WebsocketContext = createContext<Socket>(socket);
 export const WebsocketProvider = WebsocketContext.Provider;

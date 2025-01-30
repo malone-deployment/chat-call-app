8fa873ffa3895727bd7bc9eeec337f0395085b1c
fretzestavillo <estavillofretz@gmail.com>
2025-01-30 12:28:30 +0800

    test

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.PORT || 8080;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
